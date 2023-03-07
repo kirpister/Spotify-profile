5a27c1fb8c1cbedd7bee7807364b0f5c78fe9681
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2023-03-07 13:52:57 +0200

    add two more api calls and styles

---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SpotifyLogo from './assets/Spotify_Logo.png';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+           <p>All data provided by</p>
+           <a href="https://www.spotify.com/fi/signup" target="_blank" rel="noreferrer"><div className='logo-box'><img src={SpotifyLogo} alt="SpotifyLogo" /></div></a>
+        </footer>
+    );
+};
+
+export default Footer;
