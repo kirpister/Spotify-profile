7bf90ab0799cf3d7767917bbcb9df34be60c0432
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-03-14 21:06:43 +0200

    Additional formatting

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import SpotifyLogo from '../assets/Spotify_Logo.png';
+import React from "react";
+import SpotifyLogo from "../assets/Spotify_Logo.png";
 
 const Footer: React.FC = () => {
-    return (
-        <footer>
-           <p>All data provided by</p>
-           <a href="https://www.spotify.com/fi/signup" target="_blank" rel="noreferrer"><div className='logo-box'><img src={SpotifyLogo} alt="SpotifyLogo" /></div></a>
-        </footer>
-    );
+  return (
+    <footer>
+      <p>All data provided by</p>
+      <a
+        href="https://www.spotify.com/fi/signup"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="logo-box">
+          <img src={SpotifyLogo} alt="SpotifyLogo" />
+        </div>
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
